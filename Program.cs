d352f0746b0609b613b6e6172b0e8489b0f11405
Geovanne JR <Geovanne JR@LAPTOP-7A326SJL>
2023-06-01 18:49:35 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace salario
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int salariofixo;
+            double valorHora, salario;
+
+           
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salario = valorHora;
+
+            Console.WriteLine("salariofixo = " + valorHora);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
